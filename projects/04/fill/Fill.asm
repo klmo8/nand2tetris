19be6a6958ec5cf8d161c63d1e57561a25752c43
Kyle <kyelmo8@gmail.com>
2023-11-08 17:17:09 -0800

    Completed fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,61 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(INFLOOP)
+@i
+M=0
+@8192
+D=A
+@rows
+M=D
+@KBD
+D=M
+@BLACKLOOP
+D;JNE
+@WHITELOOP
+D;JEQ
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(BLACKLOOP)
+// Check loop condition
+@i
+D=M
+@rows
+D=D-M
+@INFLOOP
+D;JGE
 
-// Put your code here.
+// Black loop logic
+@i
+D=M
+@SCREEN
+A=D+A
+M=-1
+
+// Increment i
+@1
+D=A
+@i
+M=M+D
+@BLACKLOOP
+0;JMP
+
+(WHITELOOP)
+// Check loop condition
+@i
+D=M
+@rows
+D=D-M
+@INFLOOP
+D;JGE
+
+// White loop logic
+@i
+D=M
+@SCREEN
+A=D+A
+M=0
+
+// Increment i
+@1
+D=A
+@i
+M=M+D
+@WHITELOOP
+0;JMP
